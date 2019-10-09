0b92a2cc863c4585f9aaaaa18d90f27165d55857
Naumov1889storage <maksim..vr14@gmail.com>
2019-10-10 03:08:59 +0500

    Product page

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,4 @@
-$( function() {
+$(function () {
 
     $('.slider_wide').slick({
         arrows: false,
@@ -8,6 +8,26 @@ $( function() {
     $('.slider_regular').slick({
         arrows: true,
         dots: true,
+    });
+
+    var linkSpy = $('.link-spy');
+    linkSpy.click(function (e) {
+        e.preventDefault();
+
+        var currentLink = $(this)
+        var href = currentLink.attr('href');
+        var scrollTo = $(href);
+        var offsetTop = scrollTo.offset().top;
+        $('html, body').stop().animate({
+            scrollTop: offsetTop - 50
+        }, 300);
+        linkSpy.not(currentLink).removeClass('active');
+
+
+        setTimeout(function () {
+            currentLink.addClass('active');
+        }, 300);
+
     });
 
 
